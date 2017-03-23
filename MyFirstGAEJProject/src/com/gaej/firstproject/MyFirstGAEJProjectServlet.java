ac197680a82fd878d574068e2283fe23ccd185e1
tranngobao <tranngobao@gmail.com>
2017-03-23 08:03:56 +0700

    Bài tập tuần 7
    Nhóm 3

---
+++ MyFirstGAEJProject/src/com/gaej/firstproject/MyFirstGAEJProjectServlet.java
@@ -0,0 +1,12 @@
+package com.gaej.firstproject;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class MyFirstGAEJProjectServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.sendRedirect("hihi.jsp");
+
+	}
+}
